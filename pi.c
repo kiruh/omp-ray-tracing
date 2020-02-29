561058575176b488198b81ef100777250437655b
Kiril Nedelev <kiril.nedelev98@gmail.com>
2020-02-29 13:17:48 +0200

    move x declaration from loop scope to thread scope

--- pi.c
+++ pi.c
@@ -5,17 +5,18 @@ static long num_steps = 100000;
 #define NUM_THREADS 2
 void main()
 {
+    omp_set_num_threads(NUM_THREADS);
     int i;
     double sum = 0.0;
     double step = 1.0 / (double)num_steps;
-    omp_set_num_threads(NUM_THREADS);
 #pragma omp parallel
     {
+        double x;
 #pragma omp for reduction(+ \
                           : sum)
         for (i = 0; i < num_steps; i++)
         {
-            double x = (i + 0.5) * step;
+            x = (i + 0.5) * step;
             sum += 4.0 / (1.0 + x * x);
         }
     }
