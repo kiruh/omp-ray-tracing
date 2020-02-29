32356f4e20715261f6d8da32d27e937f30a3bf4c
Kiril Nedelev <kiril.nedelev98@gmail.com>
2020-02-29 13:14:31 +0200

    implement pi with pragma omp for reduction

--- pi.c
+++ pi.c
@@ -3,26 +3,22 @@
 
 static long num_steps = 100000;
 #define NUM_THREADS 2
-int main()
+void main()
 {
-    double pi = 0.0;
+    int i;
+    double sum = 0.0;
     double step = 1.0 / (double)num_steps;
     omp_set_num_threads(NUM_THREADS);
 #pragma omp parallel
     {
-        int i;
-        double x, sum = 0.0;
-        int id = omp_get_thread_num();
-        int nthrds = omp_get_num_threads();
-        for (i = id; i < num_steps; i = i + nthrds)
+#pragma omp for reduction(+ \
+                          : sum)
+        for (i = 0; i < num_steps; i++)
         {
-            x = (i + 0.5) * step;
+            double x = (i + 0.5) * step;
             sum += 4.0 / (1.0 + x * x);
         }
-#pragma omp critical
-        {
-            pi += sum * step;
-        }
     }
+    double pi = sum * step;
     printf("%f\n", pi);
 }
