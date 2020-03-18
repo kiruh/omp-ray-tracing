e5b2cc8d9620f4e9f076742e156882a636372c0c
Kiril Nedelev <nedelev.kiril@gmail.com>
2020-03-18 20:42:47 +0200

    remove comments

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
-#include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2008
-#include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
-#include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 struct Vec
-{                   // Usage: time ./smallpt 5000 && xv image.ppm
-    double x, y, z; // position, also color (r,g,b)
+{
+    double x, y, z;
     Vec(double x_ = 0, double y_ = 0, double z_ = 0)
     {
         x = x_;
         y = y_;
         z = z_;
     }
-    Vec operator+(const Vec &b) const { return Vec(x + b.x, y + b.y, z + b.z); }
-    Vec operator-(const Vec &b) const { return Vec(x - b.x, y - b.y, z - b.z); }
-    Vec operator*(double b) const { return Vec(x * b, y * b, z * b); }
-    Vec mult(const Vec &b) const { return Vec(x * b.x, y * b.y, z * b.z); }
-    Vec &norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
-    double dot(const Vec &b) const { return x * b.x + y * b.y + z * b.z; } // cross:
-    Vec operator%(Vec &b) { return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
+    Vec operator+(const Vec &b) const
+    {
+        return Vec(x + b.x, y + b.y, z + b.z);
+    }
+    Vec operator-(const Vec &b) const
+    {
+        return Vec(x - b.x, y - b.y, z - b.z);
+    }
+    Vec operator*(double b) const
+    {
+        return Vec(x * b, y * b, z * b);
+    }
+    Vec mult(const Vec &b) const
+    {
+        return Vec(x * b.x, y * b.y, z * b.z);
+    }
+    Vec &norm()
+    {
+        return *this = *this * (1 / sqrt(x * x + y * y + z * z));
+    }
+    double dot(const Vec &b) const
+    {
+        return x * b.x + y * b.y + z * b.z;
+    }
+    Vec operator%(Vec &b)
+    {
+        return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
+    }
 };
 struct Ray
 {
